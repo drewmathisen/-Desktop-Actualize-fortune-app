df325837736e6c89acee8fd6861112be412898e3
d.mathisen <drew.a.mathisen@gmail.com>
2021-10-10 10:47:18 -0600

    Created a page that generates 6 random lottery numbers

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -2,9 +2,7 @@ class MyExamplesController < ApplicationController
 
   def fortune_teller_action
     # render json: {message: "This is the fortune teller"}
-
     for_num = rand(5)
-    
     if for_num == 0
       render json: {message: "You may see a very nice cloud today"}
     elsif for_num ==1
@@ -16,14 +14,16 @@ class MyExamplesController < ApplicationController
     elsif for_num ==4
       render json: {message: "Don't let the cat out of the bag"}
     end
-
-    def lotto_number_action
-      render json: {message: "This is your lotto number!"}
-    end
-
-
-
   end
 
+  def lotto_number_action
+    lot_num1 = rand(1..60)
+    lot_num2 = rand(1..60)
+    lot_num3 = rand(1..60)
+    lot_num4 = rand(1..60)
+    lot_num5 = rand(1..60)
+    lot_num6 = rand(1..60)
+    render json: {"These are your lucky numbers": "#{lot_num1}, #{lot_num2}, #{lot_num3}, #{lot_num4}, #{lot_num5}, #{lot_num6}"}
+  end
 
 end
