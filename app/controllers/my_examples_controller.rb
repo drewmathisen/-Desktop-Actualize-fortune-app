e53f7b1d2da1b01482619c587c7876206e092feb
d.mathisen <drew.a.mathisen@gmail.com>
2021-10-10 10:37:52 -0600

    Created a page that returns a fortune

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,29 @@
 class MyExamplesController < ApplicationController
+
+  def fortune_teller_action
+    # render json: {message: "This is the fortune teller"}
+
+    for_num = rand(5)
+    
+    if for_num == 0
+      render json: {message: "You may see a very nice cloud today"}
+    elsif for_num ==1
+      render json: {message: "Watch out for warm beverages"}
+    elsif for_num ==2
+      render json: {message: "Avoid the color yellow"}
+    elsif for_num ==3
+      render json: {message: "Charge your phone!"}
+    elsif for_num ==4
+      render json: {message: "Don't let the cat out of the bag"}
+    end
+
+    def lotto_number_action
+      render json: {message: "This is your lotto number!"}
+    end
+
+
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/your_fortune", controller: "my_examples_controller", action: "forture_teller_action"
+  get "/your_fortune", controller: "my_examples", action: "fortune_teller_action"
+
+  get "/lotto_number", controller: "my_examples", action: "lotto_number_action"
 end
